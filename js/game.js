32a449ad751196332f5ed060d74721b4b86490e3
Diran <100717659+Oladiran-Dani3l@users.noreply.github.com>
2025-05-14 00:52:40 +0100

    addition of files, Initial description.

---
+++ js/game.js
@@ -0,0 +1,13 @@
+/**
+ * @file game.js
+ * @game Chicken Crossroad
+ * @author Diran Ojoodide
+ * @date 2025-05-13
+ * 
+ * @description
+ * Core game logic for the Chicken Crossroad. Handles character and obstacles movement, collision with obstacles, scoring, level progression.
+ * 
+ * @dependencies
+ * -utility.js: Utility functions
+ * -levels.js: Level configurations
+ */
